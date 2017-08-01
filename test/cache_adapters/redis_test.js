f5dea731df31e442415283fc8edb477eb53f785a
Michael Berkovich <michael@translationexchange.com>
2017-07-31 20:24:43 -0700

    better testing

--- test/cache_adapters/redis_test.js
+++ test/cache_adapters/redis_test.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2015 Translation Exchange, Inc.
+ * Copyright (c) 2017 Translation Exchange, Inc.
  *
  *  _______                  _       _   _             ______          _
  * |__   __|                | |     | | (_)           |  ____|        | |
@@ -29,44 +29,40 @@
  * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-//var redis_mock = require('redis-mock');
-//var proxyquire =  require('proxyquire')
-//
-//var assert = require("assert");
-//
-//var RedisAdapter = proxyquire('../../lib/cache_adapters/redis', { 'redis': redis_mock });
-//
-//
-//describe('Redis', function(){
-//  describe('creation', function(){
-//
-//    var adapter = new RedisAdapter({
-//      host: "localhost",
-//      port: 6379,
-//      version: 1,
-//      timeout: 3600
-//    });
-//
-//    it('should create a client', function() {
-//      assert.ok(adapter.cache)
-//    });
-//
-//    describe('fetch/store', function(){
-//      it('should correctly fetch/store a value', function(){
-//
-//
-//
-//        adapter.store("test_key", "test_value")
-//        adapter.fetch("test_key", "default", function(err,val){
-//          assert.equal(val,"test_value")
-//        })
-//      })
-//    })
-//
-//    describe('remove', function(){
-//      it('should correctly remove a value', function(){
-//
-//      })
-//    })
-//  });
-//});
+var redis_mock = require('redis-mock');
+var proxyquire =  require('proxyquire')
+
+var assert = require("assert");
+
+var RedisAdapter = proxyquire('../../lib/cache_adapters/redis', { 'redis': redis_mock });
+
+describe('Redis', function(){
+ describe('creation', function(){
+
+   var adapter = new RedisAdapter({
+     host: "localhost",
+     port: 6379,
+     version: 1,
+     timeout: 3600
+   });
+
+   it('should create a client', function() {
+     assert.ok(adapter.cache)
+   });
+
+   describe('fetch/store', function(){
+     it('should correctly fetch/store a value', function(){
+       adapter.store("test_key", "test_value");
+       adapter.fetch("test_key", "default", function(err,val){
+         assert.equal(val,"test_value")
+       })
+     })
+   });
+
+   describe('remove', function(){
+     it('should correctly remove a value', function() {
+
+     })
+   })
+ });
+});
